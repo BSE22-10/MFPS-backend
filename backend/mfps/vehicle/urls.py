fce7ea0ecdfc539f62e748311f8a6b135edaf30e
Odeke12 <trevodex@gmail.com>
2022-07-15 15:37:03 +0300

    Updated vehicle urls.py

--- backend/mfps/vehicle/urls.py
+++ backend/mfps/vehicle/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.create_list_vehicle,name='create-list-vehicle')
+    path('',views.create_list_vehicle, name='create-list-vehicle'),
+    path('<int:pk>/update',views.update_exiting_vehicle, name='update-exiting-vehicle'),
+
 ]

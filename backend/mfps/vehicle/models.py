d88611b147caabb67b513a66e8a204d0395f1077
Odeke12 <trevodex@gmail.com>
2022-07-15 09:51:59 +0300

    Updated vehicle model

--- backend/mfps/vehicle/models.py
+++ backend/mfps/vehicle/models.py
@@ -4,9 +4,9 @@ from payment.models import Payment
 
 class Vehicle(models.Model):
     number_plate = models.CharField(max_length=8)
-    arrival_time = models.DateField()
-    departing_time = models.DateField()
-    payment_id = models.ForeignKey(Payment, on_delete=models.CASCADE)
+    arrival_time = models.DateTimeField()
+    departing_time = models.DateTimeField(null=True)
+    payment_id = models.ForeignKey(Payment, on_delete=models.CASCADE,null=True)
 
 
 

d805e054f3ed591e342117514d8693e979219731
Odeke12 <trevodex@gmail.com>
2022-07-15 09:51:34 +0300

    Updated payment model

--- backend/mfps/payment/models.py
+++ backend/mfps/payment/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Payment(models.Model):
     status = models.BooleanField(default=False)
-    # vehicle_id = models.ForeignKey(Vehicle,on_delete=models.CASCADE)
+    # vehicle_id = models.ForeignKey(Vehicle,on_delete=models.CASCADE) 
 
     @property
     def bill(self):

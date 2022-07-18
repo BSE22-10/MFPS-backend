8bf0ad381f9316f7736c9b0e1df4ab3f662082f2
Odeke12 <trevodex@gmail.com>
2022-07-18 11:54:56 +0300

    Updated vehicleService

--- src/services/vehicleService.js
+++ src/services/vehicleService.js
@@ -2,6 +2,28 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+var payment = false;
+
+async function checkPaymentStatus(vehicle_id) {
+  if (payment) {
+    await prisma.payment.update({
+      where: {
+        Vehicle: {
+          connect: {
+            id: vehicle_id,
+          },
+        },
+      },
+      data: {
+        status: true,
+      },
+    });
+    return false;
+  } else {
+    return false;
+  }
+}
+
 export async function getVehicles(res) {
   const vehicles = await prisma.vehicle.findMany({});
   return vehicles;
@@ -9,22 +31,35 @@ export async function getVehicles(res) {
 
 export async function createVehicle(data) {
   console.log(data);
-  await prisma.vehicle.create({
+  const vehicle = await prisma.vehicle.create({
     data: {
       number_plate: data.number_plate,
+    },
+  });
+
+  await prisma.payment.create({
+    data: {
+      Vehicle: {
+        connect: {
+          id: vehicle.id,
+        },
+      },
     },
   });
 }
 
 export async function updateExitingVehicle(number_plate, departing_time) {
-  const getVehicleId = await prisma.vehicle.findFirst({
+  const getVehicleId = await prisma.vehicle.findMany({
     where: {
       number_plate: number_plate,
+    },
+    orderBy: {
+      arrival_time: "asc",
     },
   });
   await prisma.vehicle.update({
     where: {
-      id: getVehicleId?.id,
+      id: getVehicleId[0].id,
     },
     data: {
       departing_time: departing_time,

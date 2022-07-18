a20cdf63b6b2f3e508b9db18e8e737a7aa142103
Odeke12 <trevodex@gmail.com>
2022-07-18 16:26:52 +0300

    Added floorService

---
+++ src/services/floorService.js
@@ -0,0 +1,56 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const checkIfFloorExists = async (id) => {
+  const floor = await prisma.floor.findFirst({
+    where: {
+      id: id,
+    },
+  });
+  if (floor) {
+    return true;
+  } else {
+    throw new Error({
+      status: 400,
+      message: "Floor does not exists",
+    });
+  }
+};
+
+export const getFloors = async () => {
+  const floors = await prisma.floor.findMany({});
+  return floors;
+};
+
+export const createFloor = async (no_of_slots) => {
+  await prisma.floor.create({
+    data: {
+      no_of_slots: no_of_slots,
+    },
+  });
+};
+
+export const updateFloor = async (no_of_slots) => {
+  try {
+    await prisma.floor.update({
+      data: {
+        no_of_slots: no_of_slots,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteFloor = async (id) => {
+  try {
+    await prisma.floor.delete({
+      where: {
+        id: id,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};

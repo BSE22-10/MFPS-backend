e75ac4f842bf14540d2a5bc717c3779e64986bd3
Odeke12 <trevodex@gmail.com>
2022-08-17 23:37:39 +0300

    Updated floorController

--- src/controllers/floorController.js
+++ src/controllers/floorController.js
@@ -44,6 +44,7 @@ router.post(
 router.put(
   "/update",
   body("no_of_slots", "Provide a valid number").isNumeric(),
+  body("name", "Provide a valid string").isString(),
   query("id", "Provide a valid primary key").isNumeric(),
   async (req, res) => {
     try {
@@ -56,9 +57,7 @@ router.put(
         return res.status(400).json({ error: "Please provide an id" });
       } else {
         // [no_of_slots] = req.body;
-        res
-          .json(await updateFloor(Number(id), Number(req.body.no_of_slots)))
-          .status(201);
+        res.json(await updateFloor(req.body)).status(201);
       }
       // res.status(201).json({
       //   message: "Vehicle updated",
@@ -103,6 +102,23 @@ router.post(
           req.body.name
         )
       );
+    } catch (error) {
+      res.status(400).json({ error: error.message || error });
+    }
+  }
+);
+
+router.delete(
+  "/delete",
+  query("floor_id", "Invalid number").isNumeric(),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      console.log(req.query.floor_id);
+      res.json(await deleteFloor(Number(req.query.floor_id)));
     } catch (error) {
       res.status(400).json({ error: error.message || error });
     }

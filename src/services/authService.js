74b42b4d7fe4175fc900f48de9f181b5e0f32b5a
Odeke12 <trevodex@gmail.com>
2022-09-04 00:44:41 +0300

    Added export for auth services

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 import * as bcrypt from "bcrypt";
-import * as jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 const checkUser = async (email) => {
   const user = await prisma.admin.findFirst({
@@ -16,13 +16,14 @@ const checkUser = async (email) => {
   }
 };
 
-const signup = async (email, password) => {
+export const signup = async (email, password) => {
+  console.log(password);
   try {
     if (await checkUser(email)) {
-      const admin = await prisma.admin.create({
+      await prisma.admin.create({
         data: {
           email: email,
-          password: bcrypt.hash(password),
+          password: await bcrypt.hash(password, 10),
         },
       });
     }
@@ -34,7 +35,7 @@ const signup = async (email, password) => {
   }
 };
 
-const login = async (email, password) => {
+export const login = async (email, password) => {
   try {
     const admin = await prisma.admin.findFirst({
       where: {
@@ -44,15 +45,25 @@ const login = async (email, password) => {
     if (!admin) {
       throw new Error("User does not exist");
     } else {
-      bcrypt.compare(admin.password, password, (error, response) => {
-        if (response) {
-          const token = jwt.sign({ email }, process.env.SECRET, {
-            expiresIn: 1000,
-          });
-        } else {
-          throw new Error("Invalid credentials");
-        }
-      });
+      return bcrypt
+        .compare(password, admin.password)
+        .then((isEqual) => {
+          if (isEqual) {
+            const token = jwt.sign({ email: email }, process.env.SECRET, {
+              expiresIn: 1000,
+            });
+            userToken = token;
+            return {
+              auth: true,
+              token: token,
+            };
+          } else {
+            throw new Error("Invalid credentials");
+          }
+        })
+        .catch((error) => {
+          throw error;
+        });
     }
   } catch (error) {
     throw error;
